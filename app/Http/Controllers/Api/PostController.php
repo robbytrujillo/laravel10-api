cf92b0769ee4ba56cc5351dbb54c9a40779c93e3
robbytrujillo <robbyikusuma88@gmail.com>
2024-11-24 05:43:48 +0700

    introduction laravel10-api

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -98,5 +98,35 @@ class PostController extends Controller
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
+
+        // find post by ID
+        $post = Post::find($id);
+
+        // check if image is not empty
+        if ($request->hasFile('image')) {
+
+            // upload image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            // delete old image
+            Storage::delete('public/posts/'.basename($post->image));
+
+            // update post with new image
+            $post->update([
+                'image' => $image->hashName(),
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+        } else {
+            // update post without image
+            $post->update([
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+        }
+
+        // return response
+        return new PostResource(true, 'Data Post Berhasil diubah', $post);
     }
 }
